614757f2b8db64d1ac97c3299ed9a49fc6b5910f
Paul <paul.sinsoulieu@epitech.eu>
2023-12-18 16:43:41 +0100

    [+] Engine: Beginning of Rendering components
    Description:
    - Beginning of the class with:
    	- get and set for texture with different parameters
    	- get, set and create for sprite with different parameters
    - Temp assets for testing the component

--- Components/all_components/Rendering.cpp
+++ Components/all_components/Rendering.cpp
@@ -3,3 +3,42 @@
 //
 
 #include "Rendering.h"
+
+sf::Sprite Rendering::getSprite() const {
+    return sprite;
+}
+
+sf::Texture Rendering::getTexture() const {
+    return texture;
+}
+
+void Rendering::createSprite(const std::string& texturePath) {
+    if (texture.loadFromFile(texturePath)) {
+        sprite.setTexture(texture);
+    } else {
+        std::cerr << "Error: Texture not found" << std::endl;
+    }
+}
+
+void Rendering::createSprite(const sf::Texture& existingTexture) {
+    sprite.setTexture(existingTexture);
+}
+
+void Rendering::createSprite() {
+    sprite.setTexture(texture);
+}
+
+void Rendering::setSprite(const sf::Sprite& sprite) {
+    Rendering::sprite = sprite;
+}
+
+void Rendering::setTexture(const std::string& texturePath) {
+    if (texture.loadFromFile(texturePath)) {
+    } else {
+        std::cerr << "Error: Texture not found" << std::endl;
+    }
+}
+
+void Rendering::setTexture(const sf::Texture& existingTexture) {
+    texture = existingTexture;
+}
