105a25e5cca55eeaa9d29531d1e3d06217d478dc
Paul <paul.sinsoulieu@epitech.eu>
2023-12-18 15:50:25 +0100

    [+] Engine: Added SFML in CMakeLists
    Description:
    SFML is now found and linked in:
    - Engine/CMakeLists.txt
    - Engine/tests/CMakeLists.txt

--- Components/all_components/Rendering.h
+++ Components/all_components/Rendering.h
@@ -5,12 +5,15 @@
 #ifndef R_TYPE_RENDERING_H
 #define R_TYPE_RENDERING_H
 
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Audio.hpp>
 
 class Rendering {
 public:
     Rendering() = default;
     virtual  ~Rendering() = default;
 };
-
 
 #endif //R_TYPE_RENDERING_H
