8b7d052b6f3572e8cdf0a83618641cc6d2f4e425
Thibault Campagne <thibault.campagne@epitech.eu>
2023-12-20 11:04:16 +0100

    [m] Engine:
    Description:
    - Beginning of the World system

---
+++ World/world.cpp
@@ -0,0 +1,19 @@
+//
+// Created by thibaultcampagne on 20/12/23.
+//
+
+#include "world.h"
+
+World::World(std::string type, sf::VideoMode mode, const std::string &title, sf::Uint32 style,
+const sf::ContextSettings &settings) {
+    if (type == "2D" or type == "2d") {
+        sf::RenderWindow(mode, title, style, settings);
+    } else if (type == "3D" or type == "3d") {
+        sf::Window(mode, title, style, settings);
+    } else {
+        std::cout << "Veuillez choisir un type de graphisme entre 2D et 3D" << std::endl;
+    }
+}
+
+void World::createEntities(std::vector<Entity> entities, std::map<std::string, std::vector<std::string>>) {
+}
