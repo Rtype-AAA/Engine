c9002e1cbf1b2108d989f90ec769803f462e313d
Paul <paul.sinsoulieu@epitech.eu>
2024-01-09 15:43:28 +0100

    [~] Engine: Update quantumvortex-engineConfig to add include dirs and build-deb.yml to fix the fact that the .deb wasn't in the zip

--- configPackage/quantumvortex-engineConfig.cmake
+++ configPackage/quantumvortex-engineConfig.cmake
@@ -3,3 +3,5 @@ include(CMakeFindDependencyMacro)
 find_package(SFML COMPONENTS graphics window system audio CONFIG REQUIRED)
 
 include("${CMAKE_CURRENT_LIST_DIR}/quantumvortex-engineTargets.cmake")
+
+set(quantumvortex_engine_INCLUDE_DIRS ${quantumvortex_engine_INCLUDE_DIRS} PARENT_SCOPE)
