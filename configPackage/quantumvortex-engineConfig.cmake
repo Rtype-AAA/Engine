8eb1934f0935f4acc956489124ba1f577e3851de
Paul <paul.sinsoulieu@epitech.eu>
2024-01-10 21:06:03 +0100

    [~] Completly modified the method to build .deb package

--- configPackage/quantumvortex-engineConfig.cmake
+++ configPackage/quantumvortex-engineConfig.cmake
@@ -4,4 +4,4 @@ find_package(SFML COMPONENTS graphics window system audio CONFIG REQUIRED)
 
 include("${CMAKE_CURRENT_LIST_DIR}/quantumvortex-engineTargets.cmake")
 
-set(quantumvortex_engine_INCLUDE_DIRS ${quantumvortex_engine_INCLUDE_DIRS} PARENT_SCOPE)
+set(quantumvortex-engine_INCLUDE_DIRS ${quantumvortex-engine_INCLUDE_DIRS} PARENT_SCOPE)
