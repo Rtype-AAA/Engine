27f766b6331a2eecc73e935d4b9c469e3041edeb
Paul <paul.sinsoulieu@epitech.eu>
2024-01-12 17:40:14 +0100

    [~] Engine: Updated Google Tests for Entity

--- tests/Entity/TestEntity.cpp
+++ tests/Entity/TestEntity.cpp
@@ -20,7 +20,7 @@ TEST_F(EntityTest, ConstructorWithName) {
     EXPECT_EQ("TestEntity", entity.getName());
 }
 
-TEST_F(EntityTest, Init) {
+TEST_F(EntityTest, InitEntity) {
     EXPECT_TRUE(entity.initEntity());
 }
 
@@ -31,17 +31,65 @@ TEST_F(EntityTest, GetNameAndSetName) {
     EXPECT_EQ("TestEntity", entity.getName());
 }
 
+TEST_F(EntityTest, Update) {
+    EXPECT_NO_THROW(entity.update(sf::Time::Zero));
+}
+
+TEST_F(EntityTest, addDrawable) {
+    EXPECT_NO_THROW(entity.addDrawable(new Sprite()));
+}
+
 TEST_F(EntityTest, AddAndGetComponent) {
-    std::map<std::string, std::vector<float>> transformData;
+    entity.addComponent<Transform>();
 
-    transformData["Position"] = std::vector<float>{1.0f, 2.0f};
-    transformData["Rotation"] = std::vector<float>{3.0f, 4.0f};
-    transformData["Scale"] = std::vector<float>{5.0f, 6.0f};
+    EXPECT_EQ(0, entity.getComponent<Transform>().getBit());
 
-    Transform addedComponent(transformData);
+    entity1.addComponent<Transform>();
+
+    entity1.getComponent<Transform>().setTransform(Vector2<float>(1.0f, 1.0f), 1.0f, Vector2<float>(2.0f, 2.0f));
+
+    EXPECT_EQ(0, entity1.getComponent<Transform>().getBit());
+
+    EXPECT_EQ(1, entity1.getComponent<Transform>().getTransformStruct().position.getX());
+    EXPECT_EQ(1, entity1.getComponent<Transform>().getTransformStruct().position.getY());
+    EXPECT_EQ(1, entity1.getComponent<Transform>().getTransformStruct().rotation);
+    EXPECT_EQ(2, entity1.getComponent<Transform>().getTransformStruct().scale.getX());
+    EXPECT_EQ(2, entity1.getComponent<Transform>().getTransformStruct().scale.getY());
+
+    entity1.addComponent<Sprite>();
+
+    EXPECT_EQ(1, entity1.getComponent<Sprite>().getBit());
+}
+
+TEST_F(EntityTest, GetComponentTypeID) {
+    entity.addComponent<Transform>();
+    entity.addComponent<Sprite>();
+
+    EXPECT_EQ(0, entity.getComponent<Transform>().getBit());
+    EXPECT_EQ(1, entity.getComponent<Sprite>().getBit());
+}
+
+TEST_F(EntityTest, GetDrawableComponents) {
+    entity.addComponent<Transform>();
+    entity.addComponent<Sprite>();
+
+    EXPECT_EQ(0, entity.getComponent<Transform>().getBit());
+    EXPECT_EQ(1, entity.getComponent<Sprite>().getBit());
+
+    EXPECT_EQ(1, entity.getDrawableComponents().size());
+
+    entity.addComponent<Sprite>();
+
+    EXPECT_EQ(2, entity.getDrawableComponents().size());
+}
+
+TEST_F(EntityTest, GetComponentArrays) {
+    EXPECT_EQ(3, entity.getComponentArrays().size());
 
     entity.addComponent<Transform>();
-    entity1.addComponent<Transform>(transformData);
+    entity.addComponent<Sprite>();
 
-    EXPECT_EQ(addedComponent.getBit(), entity.getComponent<Transform>().getBit());
+    EXPECT_EQ(3, entity.getComponentArrays().size());
+
+    std::array<Components*, 3> componentArray = entity.getComponentArrays();
 }
