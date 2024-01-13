8aa898814d57f2e325afedeeceaab4769d854381
Paul <paul.sinsoulieu@epitech.eu>
2024-01-14 00:15:15 +0100

    [~] Engine: Updated GoogleTests and Documentation for Entity

--- tests/Entity/TestEntity.cpp
+++ tests/Entity/TestEntity.cpp
@@ -1,4 +1,5 @@
 #include "gtest/gtest.h"
+#include <gmock/gmock.h>
 #include "entityManager.h"
 #include "entity.h"
 #include "Transform.h"
@@ -20,8 +21,12 @@ TEST_F(EntityTest, ConstructorWithName) {
     EXPECT_EQ("TestEntity", entity.getName());
 }
 
-TEST_F(EntityTest, InitEntity) {
-    EXPECT_TRUE(entity.initEntity());
+TEST_F(EntityTest, GetBit) {
+    EXPECT_EQ(0, entity.getBit());
+}
+
+TEST_F(EntityTest, Init) {
+    EXPECT_TRUE(entity.init());
 }
 
 TEST_F(EntityTest, GetNameAndSetName) {
@@ -29,6 +34,9 @@ TEST_F(EntityTest, GetNameAndSetName) {
 
     entity.setName("TestEntity");
     EXPECT_EQ("TestEntity", entity.getName());
+
+    entity.setName("TestEntity1");
+    EXPECT_EQ("TestEntity1", entity.getName());
 }
 
 TEST_F(EntityTest, Update) {
@@ -37,6 +45,20 @@ TEST_F(EntityTest, Update) {
 
 TEST_F(EntityTest, addDrawable) {
     EXPECT_NO_THROW(entity.addDrawable(new Sprite()));
+}
+
+TEST_F(EntityTest, RemoveDrawable) {
+    entity.addComponent<Transform>();
+    entity.addComponent<Sprite>();
+
+    EXPECT_EQ(0, entity.getComponent<Transform>().getBit());
+    EXPECT_EQ(1, entity.getComponent<Sprite>().getBit());
+
+    EXPECT_EQ(1, entity.getDrawableComponents().size());
+
+    entity.removeDrawable(&entity.getComponent<Sprite>());
+
+    EXPECT_EQ(0, entity.getDrawableComponents().size());
 }
 
 TEST_F(EntityTest, AddAndGetComponent) {
@@ -50,15 +72,34 @@ TEST_F(EntityTest, AddAndGetComponent) {
 
     EXPECT_EQ(0, entity1.getComponent<Transform>().getBit());
 
-    EXPECT_EQ(1, entity1.getComponent<Transform>().getTransformStruct().position.getX());
-    EXPECT_EQ(1, entity1.getComponent<Transform>().getTransformStruct().position.getY());
-    EXPECT_EQ(1, entity1.getComponent<Transform>().getTransformStruct().rotation);
-    EXPECT_EQ(2, entity1.getComponent<Transform>().getTransformStruct().scale.getX());
-    EXPECT_EQ(2, entity1.getComponent<Transform>().getTransformStruct().scale.getY());
+    EXPECT_EQ(1, entity1.getComponent<Transform>().getTransform().position.getX());
+    EXPECT_EQ(1, entity1.getComponent<Transform>().getTransform().position.getY());
+    EXPECT_EQ(1, entity1.getComponent<Transform>().getTransform().rotation);
+    EXPECT_EQ(2, entity1.getComponent<Transform>().getTransform().scale.getX());
+    EXPECT_EQ(2, entity1.getComponent<Transform>().getTransform().scale.getY());
 
     entity1.addComponent<Sprite>();
 
     EXPECT_EQ(1, entity1.getComponent<Sprite>().getBit());
+}
+
+TEST_F(EntityTest, RemoveComponent) {
+    EXPECT_EQ(6, entity.getComponentArrays().size());
+    EXPECT_EQ(0, entity.getDrawableComponents().size());
+
+    entity.addComponent<Transform>();
+    entity.addComponent<Sprite>();
+
+    EXPECT_EQ(0, entity.getComponent<Transform>().getBit());
+    EXPECT_EQ(1, entity.getComponent<Sprite>().getBit());
+
+    EXPECT_EQ(6, entity.getComponentArrays().size());
+    EXPECT_EQ(1, entity.getDrawableComponents().size());
+
+    entity.removeComponent<Sprite>();
+
+    EXPECT_EQ(0, entity.getDrawableComponents().size());
+    EXPECT_EQ(6, entity.getComponentArrays().size());
 }
 
 TEST_F(EntityTest, GetComponentTypeID) {
@@ -67,6 +108,18 @@ TEST_F(EntityTest, GetComponentTypeID) {
 
     EXPECT_EQ(0, entity.getComponent<Transform>().getBit());
     EXPECT_EQ(1, entity.getComponent<Sprite>().getBit());
+}
+
+TEST_F(EntityTest, GetComponentBitset) {
+    EXPECT_EQ(6, entity.getComponentBitset().size());
+
+    entity.addComponent<Transform>();
+    entity.addComponent<Sprite>();
+
+    EXPECT_EQ(0, entity.getComponent<Transform>().getBit());
+    EXPECT_EQ(1, entity.getComponent<Sprite>().getBit());
+
+    EXPECT_EQ(6, entity.getComponentBitset().size());
 }
 
 TEST_F(EntityTest, GetDrawableComponents) {
@@ -84,12 +137,50 @@ TEST_F(EntityTest, GetDrawableComponents) {
 }
 
 TEST_F(EntityTest, GetComponentArrays) {
-    EXPECT_EQ(3, entity.getComponentArrays().size());
+    EXPECT_EQ(6, entity.getComponentArrays().size());
 
     entity.addComponent<Transform>();
     entity.addComponent<Sprite>();
 
-    EXPECT_EQ(3, entity.getComponentArrays().size());
+    EXPECT_EQ(6, entity.getComponentArrays().size());
 
-    std::array<Components*, 3> componentArray = entity.getComponentArrays();
+    std::array<Components*, 6> componentArray = entity.getComponentArrays();
+}
+
+TEST_F(EntityTest, SetActive) {
+    EXPECT_TRUE(entity.getActive());
+
+    entity.setActive(false);
+
+    EXPECT_FALSE(entity.getActive());
+
+    entity.setActive(true);
+
+    EXPECT_TRUE(entity.getActive());
+}
+
+TEST_F(EntityTest, GetActive) {
+    EXPECT_TRUE(entity.getActive());
+
+    entity.setActive(false);
+
+    EXPECT_FALSE(entity.getActive());
+
+    entity.setActive(true);
+
+    EXPECT_TRUE(entity.getActive());
+}
+
+TEST_F(EntityTest, SetAndApplyDeferredEntity) {
+    auto helloWorldFunction = []() { std::cout << "Hello world" << std::endl; };
+
+    entity.setDeferredEntity(helloWorldFunction);
+
+    testing::internal::CaptureStdout();
+
+    entity.applyDeferredEntity();
+
+    std::string output = testing::internal::GetCapturedStdout();
+
+    ASSERT_THAT(output, ::testing::HasSubstr("Hello world"));
 }
